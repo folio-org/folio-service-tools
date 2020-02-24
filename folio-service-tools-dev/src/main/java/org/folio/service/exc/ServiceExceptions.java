79228f900e440470d08274632d5d9d5963153556
dmtkachenko <42244720+dmtkachenko@users.noreply.github.com>
2020-02-24 16:44:00 +0200

    - introduce general invalid data exception - introduce invalid field value exception

--- folio-service-tools-dev/src/main/java/org/folio/service/exc/ServiceExceptions.java
+++ folio-service-tools-dev/src/main/java/org/folio/service/exc/ServiceExceptions.java
@@ -1,12 +1,12 @@
 package org.folio.service.exc;
 
 import static java.lang.String.format;
-
 import javax.ws.rs.NotFoundException;
 
 public class ServiceExceptions {
 
   private static final String NOT_FOUND_MSG = "%s not found by id: %s";
+  private static final String INVALID_FIELD_VALUE = "'%s' field value is invalid: %s";
 
   private ServiceExceptions() {
   }
@@ -17,5 +17,9 @@ public class ServiceExceptions {
 
   public static NotFoundException notFound(Class<?> entityClass, String id) {
     return notFound(entityClass.getSimpleName(), id);
+  }
+
+  public static InvalidFieldValueException invalidValue(String field, Object value) {
+    return new InvalidFieldValueException(field, value, format(INVALID_FIELD_VALUE, field, value));
   }
 }
