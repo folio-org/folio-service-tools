ca10c95bf8cef5c7c719f6cc9f9c96c93cf9e523
Pavel Smagin <Pavlo_Smahin@epam.com>
2020-12-21 14:27:17 +0200

    FST-6 Update TestSetUpHelper to wait until tenant deployed (#48)

--- folio-service-tools-test/src/main/java/org/folio/test/util/TestSetUpHelper.java
+++ folio-service-tools-test/src/main/java/org/folio/test/util/TestSetUpHelper.java
@@ -14,12 +14,15 @@ import io.vertx.core.json.JsonObject;
 import org.folio.rest.RestVerticle;
 import org.folio.rest.client.TenantClient;
 import org.folio.rest.jaxrs.model.TenantAttributes;
+import org.folio.rest.jaxrs.model.TenantJob;
 import org.folio.rest.persist.PostgresClient;
 import org.folio.rest.tools.PomReader;
 import org.folio.rest.tools.utils.NetworkUtils;
 
 public class TestSetUpHelper {
+
   private static final String HTTP_PORT = "http.port";
+  private static final int TENANT_OP_WAITINGTIME = 60000;
 
   private static int port;
   private static String host;
@@ -41,16 +44,30 @@ public class TestSetUpHelper {
       // or use the one from env properties
       pgClient = PostgresClient.getInstance(vertx);
     } catch (Exception e) {
-      throw new IllegalStateException("Failed to initialize postgres client" , e);
+      throw new IllegalStateException("Failed to initialize postgres client", e);
     }
 
     CompletableFuture<Void> future = new CompletableFuture<>();
     vertx.deployVerticle(RestVerticle.class.getName(), getDeploymentOptions(configProperties), event -> {
       TenantClient tenantClient = new TenantClient(host + ":" + port, STUB_TENANT, STUB_TOKEN);
       try {
-        tenantClient.postTenant(new TenantAttributes().withModuleTo(PomReader.INSTANCE.getVersion()), res2 -> future.complete(null));
+        TenantAttributes tenantAttributes = new TenantAttributes().withModuleTo(PomReader.INSTANCE.getVersion());
+        tenantClient.postTenant(tenantAttributes, res1 -> {
+          if (res1.succeeded()) {
+            String jobId = res1.result().bodyAsJson(TenantJob.class).getId();
+            tenantClient.getTenantByOperationId(jobId, TENANT_OP_WAITINGTIME, res2 -> {
+              if (res2.succeeded()) {
+                future.complete(null);
+              } else {
+                future.completeExceptionally(new IllegalStateException("Failed to get tenant"));
+              }
+            });
+          } else {
+            future.completeExceptionally(new IllegalStateException("Failed to create tenant job"));
+          }
+        });
       } catch (Exception e) {
-        e.printStackTrace();
+        future.completeExceptionally(e);
       }
     });
     future.join();
