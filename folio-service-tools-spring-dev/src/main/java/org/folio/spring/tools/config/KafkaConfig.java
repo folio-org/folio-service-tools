b47a8fc9fd9ff18d2f9d62ef5fca0018d531ce27
Pavlo Smahin <Pavlo_Smahin@epam.com>
2022-11-02 21:34:29 +0200

    FST-34 Change condition on KafkaConfig

--- folio-service-tools-spring-dev/src/main/java/org/folio/spring/tools/config/KafkaConfig.java
+++ folio-service-tools-spring-dev/src/main/java/org/folio/spring/tools/config/KafkaConfig.java
@@ -1,13 +1,16 @@
 package org.folio.spring.tools.config;
 
+import org.folio.spring.tools.config.condition.OnKafkaTopicsPropertyCondition;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Conditional;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.kafka.core.KafkaTemplate;
 
 @Configuration
 @ConditionalOnClass({KafkaTemplate.class})
-@ConditionalOnProperty(prefix = "folio.kafka", name = "topics")
+@Conditional(OnKafkaTopicsPropertyCondition.class)
 @ComponentScan(basePackages = "org.folio.spring.tools.kafka")
-public class KafkaConfig { }
+public class KafkaConfig {
+
+}
