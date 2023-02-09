e6ed1222c9acd88202ff4c306fb013443d77d00d
julianladisch <julianladisch@users.noreply.github.com>
2023-02-09 17:02:38 +0100

    FST-41: DbUtils transaction: Explain deprecation, fix unit tests (#82)
    * FST-41: DbUtils transaction: Explain deprecation, fix unit tests
    
    * Use folio-spring-base 6.0.0, not 6.0.0-SNAPSHOT
    
    (cherry picked from commit 887d8a94779fa725d5c69275a71069d6e87384a8)
    
    ---------
    
    Co-authored-by: Pavlo Smahin <Pavlo_Smahin@epam.com>
    (cherry picked from commit 728ff0a9e91935602370f3440d95d0befb7dfa1f)

--- folio-service-tools-dev/src/test/java/org/folio/rest/tools/utils/ModuleName.java
+++ folio-service-tools-dev/src/test/java/org/folio/rest/tools/utils/ModuleName.java
@@ -2,6 +2,6 @@ package org.folio.rest.tools.utils;
 
 public class ModuleName {
   public static String getModuleName() {
-    return "unit-test-example-module";
+    return "unit_test_example_module";
   }
 }
