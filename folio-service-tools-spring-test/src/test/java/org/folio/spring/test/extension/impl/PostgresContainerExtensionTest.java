3258ddd3b01ce44437a2c23b858ab36c61866b17
Julian Ladisch <julianladisch@users.noreply.github.com>
2023-11-14 15:29:01 +0100

    FST-69: Implement TESTCONTAINERS_POSTGRES_IMAGE
    Spec: https://wiki.folio.org/display/TC/DR-000037+-+TESTCONTAINERS_POSTGRES_IMAGE

--- folio-service-tools-spring-test/src/test/java/org/folio/spring/test/extension/impl/PostgresContainerExtensionTest.java
+++ folio-service-tools-spring-test/src/test/java/org/folio/spring/test/extension/impl/PostgresContainerExtensionTest.java
@@ -2,7 +2,13 @@ package org.folio.spring.test.extension.impl;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+import java.util.Map;
+import java.util.stream.Stream;
+
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 class PostgresContainerExtensionTest {
 
@@ -20,5 +26,19 @@ class PostgresContainerExtensionTest {
   void afterAllAddSystemProperties_positive() {
     extension.afterAll(null);
     assertThat(System.getProperty("spring.datasource.url")).isNull();
+  }
+
+  @ParameterizedTest
+  @MethodSource
+  void postgresImage(Map<String, String> env, String expected) {
+    assertThat(PostgresContainerExtension.postgresImage(env)).isEqualTo(expected);
+  }
+
+  static Stream<Arguments> postgresImage() {
+    return Stream.of(
+        Arguments.of(Map.of(), "postgres:12-alpine"),
+        Arguments.of(Map.of("foo", "bar"), "postgres:12-alpine"),
+        Arguments.of(Map.of("foo", "bar", "TESTCONTAINERS_POSTGRES_IMAGE", "x:y-z"), "x:y-z")
+        );
   }
 }
